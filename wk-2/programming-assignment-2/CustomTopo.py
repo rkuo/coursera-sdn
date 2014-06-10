b21423de2657f5d68cdf3d97f2493b66800e4138
Richard Kuo <kuotie@gmail.com>
2014-06-10 15:58:43 -0700

    node and link names generated

--- wk-2/programming-assignment-2/CustomTopo.py
+++ wk-2/programming-assignment-2/CustomTopo.py
@@ -1,42 +1,45 @@
-'''
-Coursera:
-- Software Defined Networking (SDN) course
--- Programming Assignment 2
+#!/usr/bin/python
 
-Professor: Nick Feamster
-Teaching Assistant: Arpit Gupta, Muhammad Shahbaz
-'''
-
-from mininet.topo import Topo
-
-class CustomTopo(Topo):
-    "Simple Data Center Topology"
-
-    "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
-    "fanout - number of child switch per parent switch"
-    def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
-        # Initialize topology and default options
-        Topo.__init__(self, **opts)
-        
-        # Add your logic here ...
-
-# need testing code here
-def simpleTest():
-	# create and test simple network
-	# bw is in mps, delay is in ms or us, loss is in percentag, 
-	linkopts1 = {'bw': 30, 'delay': '20ms'}
-	linkopts2 = {'bw': 20, 'delay': '10ms'}
-	linkopts3 = {'bw': 10, 'delay': '10ms'}
-	fanout = 2
-	
-	topo = CustomTopo(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts)
+pfx = ['c', 'a', 'e', 'h']
+fo = [2, 2, 2, 0]
+lo = ['l1', 'l2', 'l3', '']
 
 
-# need this code for running local
-if __name__ == 'main':
-	# tell mininet to print useful information
-	setLogLevel('info')
-	simpleTest()
-        
-# this will be used for running on command line, unblock it for submit.py                    
-# topos = { 'custom': ( lambda: CustomTopo() ) }
+# set part of node label = 1
+csw_num = asw_num = esw_num = host_num = 1
+
+# create core
+pn = pfx[0]					# parent node
+node_name = "c%s" %csw_num	# if more core sw; csw_num += 1
+print node_name
+# create core sw
+# node = self.addSwitch(node_name)
+
+p_node = node_name
+
+for i in range(fo[0]):
+    node_name = "a%s" %asw_num
+    print node_name; asw_num += 1
+    # create a sw
+    # node = self.addSwitch(node_name)
+
+    print p_node + " - " + node_name            # makelink
+    p_node = node_name
+
+    for j in range(fo[1]):
+        node_name = "e%s" %esw_num;
+        print node_name; esw_num += 1
+        # create a sw
+        # node = self.addSwitch(node_name)
+        print p_node + " - " + node_name
+        p_node = node_name
+
+        for k in range(fo[2]):
+            node_name = "h%s" %(host_num)
+            print node_name; host_num += 1
+            # create host
+            # node = self.addHost(node_name)
+            print p_node + " - " + node_name
+
+
+
