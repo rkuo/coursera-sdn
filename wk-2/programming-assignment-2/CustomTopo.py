054f8dd90be4d876950f68409b72c22d283329bd
Richard Kuo <kuotie@gmail.com>
2014-06-11 15:41:37 -0700

    added ** infront of linkopts1, submitted

--- wk-2/programming-assignment-2/CustomTopo.py
+++ wk-2/programming-assignment-2/CustomTopo.py
@@ -1,45 +1,90 @@
 #!/usr/bin/python
 
-pfx = ['c', 'a', 'e', 'h']
-fo = [2, 2, 2, 0]
-lo = ['l1', 'l2', 'l3', '']
+'''
+Coursera:
+- Software Defined Networking (SDN) course
+-- Module 2 Programming Assignment
+'''
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink
+
+class CustomTopo(Topo):
+    "Simple Data Center Topology"
+
+    "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
+    "fanout - number of child switch per parent switch"
+    def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
+        # Initialize topology and default options
+        print "in class before init"
+        Topo.__init__(self, **opts)
+        print "in class after init"
+
+        # Add your logic here ...
+        """
+        self.linkopts1 = linkopts1
+        self.linkopts2 = linkopts2
+        self.linkopts3 = linkopts3
+        self.fanout = fanout
+        """
+
+        pfx = ['c', 'a', 'e', 'h']
+        fo = [2, 2, 2, 0]
+        lo = ['l1', 'l2', 'l3', '']
 
 
-# set part of node label = 1
-csw_num = asw_num = esw_num = host_num = 1
+        # set part of node label = 1
+        n_num = [0] * len(pfx)              # init for node label-numbering
 
-# create core
-pn = pfx[0]					# parent node
-node_name = "c%s" %csw_num	# if more core sw; csw_num += 1
-print node_name
-# create core sw
-# node = self.addSwitch(node_name)
+        # create core
+        n_num[0] += 1	    		        # node number
+        c_node_name = pfx[0]+"%s" %n_num[0]
+        print "core sw %s" %c_node_name
+        self.addSwitch(c_node_name)
 
-p_node = node_name
+        for i in range(fanout):
+            n_num[1] += 1	    		    # node number
+            a_node_name = pfx[1]+"%s" %n_num[1]
+            print "  aggr sw %s" %a_node_name
+            self.addSwitch(a_node_name)
 
-for i in range(fo[0]):
-    node_name = "a%s" %asw_num
-    print node_name; asw_num += 1
-    # create a sw
-    # node = self.addSwitch(node_name)
+            print "    link %s - %s" %(c_node_name, a_node_name)
+            self.addLink(c_node_name, a_node_name, **linkopts1)
 
-    print p_node + " - " + node_name            # makelink
-    p_node = node_name
+            for j in range(fanout):
+                n_num[2] += 1	    		# node number
+                e_node_name = pfx[2]+"%s" %n_num[2]
+                print "    edge sw %s" %e_node_name
+                self.addSwitch(e_node_name)
 
-    for j in range(fo[1]):
-        node_name = "e%s" %esw_num;
-        print node_name; esw_num += 1
-        # create a sw
-        # node = self.addSwitch(node_name)
-        print p_node + " - " + node_name
-        p_node = node_name
+                print "    link %s - %s" %(a_node_name, e_node_name)
+                self.addLink(a_node_name, e_node_name, **linkopts2)
 
-        for k in range(fo[2]):
-            node_name = "h%s" %(host_num)
-            print node_name; host_num += 1
-            # create host
-            # node = self.addHost(node_name)
-            print p_node + " - " + node_name
+                for k in range(fanout):
+                    n_num[3] += 1	    		# node number
+                    h_node_name = pfx[3]+"%s" %n_num[3]
+                    print "      host  %s" %h_node_name
+                    self.addHost(h_node_name)
+                    print "       link %s - %s" %(e_node_name, h_node_name)
+                    self.addLink(e_node_name, h_node_name, **linkopts3)
 
+topos = { 'custom': ( lambda: CustomTopo() ) }
+
+if __name__ == '__main__':
+
+    def simpleTest():
+        linkopts1 = {'bw':50, 'delay':'5ms'}
+        linkopts2 = {'bw':30, 'delay':'10ms'}
+        linkopts3 = {'bw':10, 'delay':'15ms'}
+        topo = CustomTopo(linkopts1, linkopts2, linkopts3, fanout=3)
+        net = Mininet(topo=topo, link=TCLink)
+        net.start()
+        net.pingAll()
+        print net.hosts
+        print net.switches
+        net.stop
+
+    simpleTest()
 
 
